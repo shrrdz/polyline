7bbbe1f19c061f75b75ce85a797421e0336e07db
shredz <shredz@centrum.cz>
2022-12-07 16:58:01 +0100

    add input handling

--- src/App.java
+++ src/App.java
@@ -1,10 +1,20 @@
 import geometry.Point;
 import rasterization.LineRasterizer;
 import video.Window;
+import shape.Polygon;
+
+import java.awt.event.*;
+import java.util.ArrayList;
 
 public class App
 {
+    private enum Input { LEFT, RELEASED };
+
+    private Input input;
+
     private LineRasterizer lineRasterizer;
+
+    private final Polygon polygon = new Polygon();
 
     private final Window window = new Window(800, 600);
     
@@ -12,6 +22,7 @@ public class App
     {
         lineRasterizer = new LineRasterizer(window.image);
 
+        initialize();
         update();
     }
 
@@ -19,16 +30,83 @@ public class App
     {
         window.clear(0x050505);
 
-        rasterizeLine(new Point(0, 0), new Point(799, 599), 0x6097BA);
+        rasterizeLine(polygon.getPoints(), 0x6097BA);
 
         window.update();
     }
 
-    private void rasterizeLine(Point p0, Point p1, int color)
+    private void rasterizeLine(ArrayList<Point> points, int color)
     {
-        lineRasterizer.setColor(color);
+        if (points.size() > 1)
+        {
+            lineRasterizer.setColor(color);
 
-        lineRasterizer.rasterize(p0.x, p0.y, p1.x, p1.y);
+            for (int i = 0; i < points.size(); i++)
+            {
+                lineRasterizer.rasterize(points.get(i).x, points.get(i).y,
+                    points.get((i + 1) % points.size()).x, points.get((i + 1) % points.size()).y);
+            }
+        }
+    }
+
+    private void initialize()
+    {
+        KeyListener keyListener = new KeyAdapter()
+        {
+            @Override
+            public void keyPressed(KeyEvent event)
+            {
+                if (input != Input.LEFT)
+                {
+                    switch (event.getKeyCode())
+                    {
+                        case KeyEvent.VK_C: polygon.clearPoints(); break;
+
+                        case KeyEvent.VK_ESCAPE: window.close();
+                    }
+
+                    update();
+                }
+            }
+        };
+
+        MouseAdapter mouseAdapter = new MouseAdapter()
+        {
+            @Override
+            public void mousePressed(MouseEvent event)
+            {
+                if (event.getButton() == 1)
+                {
+                    input = Input.LEFT;
+                }
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent event)
+            {
+                switch (event.getButton())
+                {
+                    case 1: polygon.addPoints(new Point(event.getX(), event.getY())); break;
+                }
+
+                input = Input.RELEASED;
+
+                update();
+            }
+        };
+
+        MouseMotionAdapter mouseMotionAdapter = new MouseMotionAdapter()
+        {
+            @Override
+            public void mouseMoved(MouseEvent event)
+            {
+                update();
+            }
+        };
+
+        window.panel.addKeyListener(keyListener);
+        window.panel.addMouseListener(mouseAdapter);
+        window.panel.addMouseMotionListener(mouseMotionAdapter);
     }
 
     public static void main(String[] args)
